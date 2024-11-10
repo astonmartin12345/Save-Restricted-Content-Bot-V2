9c5fda97e52171e0d0aa05bcdac7cea5f89f2846
astonmartin12345 <165018596+astonmartin12345@users.noreply.github.com>
2024-11-10 21:12:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "12777841"))
+API_HASH = getenv("API_HASH", "d01bbf996325918c7ceec53c0ebf1499")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6459837145").split()))
 MONGO_DB = getenv("MONGO_DB", "")
 LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "mongodb+srv://aston420martin:elNrd24uMqFSQeyW@cluster0.ktluu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
